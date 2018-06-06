5851c417771f31ac338079779da1b64c178e44f7
linshuling <1611074393@qq.com>
2018-06-06 16:21:54 +0800

    vue动态路由实现test

---
+++ src/views/GoodsList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        this is GoodsList
+        <p>{{msg}}</p>
+        <p>{{$route.params.goodsId}}</p>
+        <p>{{$route.params.name}}</p>
+    </div>
+</template>
+<style>
+    
+</style>
+<script>
+    export default{
+        data(){
+            return {
+                msg : 'linshuling'
+            }
+        }
+    }
+</script>
