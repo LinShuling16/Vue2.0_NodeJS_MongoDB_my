f8b10fed08eb9cd50585b118e29bc236fafc15c8
linshuling <1611074393@qq.com>
2018-06-06 16:28:58 +0800

    路由模式设置test——hash or history

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,13 @@ import GoodsList from '@/views/GoodsList'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/goods/:goodsId/user/:name',
-      name: 'GoodsList',
-      component: GoodsList
-    }
-  ]
+    /*这里mode可以设置路由模式，默认是哈希hash(#),也可以设置为history*/
+    mode  :'history',
+    routes: [
+        {
+          path: '/goods/:goodsId/user/:name',
+          name: 'GoodsList',
+          component: GoodsList
+        }
+    ]
 })
