838611ceef2d7420e296d5bbe7b0700eb592bb27
linshuling <1611074393@qq.com>
2018-06-12 11:10:58 +0800

    express 路由

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+//路由文件里的是二级路由
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/test', function(req, res, next) {
+  res.send('test');
 });
 
 module.exports = router;
